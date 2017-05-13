f18b94250e1cbd93e5c9b9a216983e4924de2474
Graciela Banegas <Graciela Banegas@DESKTOP-1M9QKTV>
2017-05-13 19:56:33 -0300

    arrumando controlador relatorio acessos

--- br.ufsc.ine5605.SisClaviculario/src/br/ufsc/ine5605/sisclaviculario/controle/ControladorRelatorioAcesso.java
+++ br.ufsc.ine5605.SisClaviculario/src/br/ufsc/ine5605/sisclaviculario/controle/ControladorRelatorioAcesso.java
@@ -1,13 +1,21 @@
 
 package br.ufsc.ine5605.sisclaviculario.controle;
 import br.ufsc.ine5605.sisclaviculario.entidade.RelatorioAcesso;
+import br.ufsc.ine5605.sisclaviculario.tela.TelaRelatorioAcesso;
+import br.ufsc.ine5605.sisclaviculario.controle.ControladorPrincipal;
+import br.ufsc.ine5605.sisclaviculario.tela.TelasCabecalho;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class ControladorRelatorioAcesso {
     
     private static ControladorRelatorioAcesso INSTANCE;
+    private TelasCabecalho telasCabecalho;
+    private Scanner teclado;
+    
     public ArrayList<RelatorioAcesso> listaRelatoriosAcesso;
+    
     
     private ControladorRelatorioAcesso(){
         this.listaRelatoriosAcesso = new ArrayList<>();
@@ -38,73 +46,11 @@ public class ControladorRelatorioAcesso {
         ControladorPrincipal.getINSTANCE().inicia();
     }
     
-}
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- 
-package br.ufsc.ine5605.sisclaviculario.controle;
-import br.ufsc.ine5605.sisclaviculario.tela.TelaRelatorioAcesso;
-import br.ufsc.ine5605.sisclaviculario.tela.TelasCabecalho;
-import java.util.Scanner;
-
-
- *
- * @author Graciela Banegas
- 
-public class ControladorRelatorioAcesso {
-
-    static ControladorRelatorioAcesso INSTANCE;
-    
-    
-    private TelaRelatorioAcesso telaRelatorioAcesso;
-    private TelasCabecalho telasCabecalho;
-    private ControladorPrincipal ctrlP;
-    private Scanner teclado;
-    public static ControladorRelatorioAcesso ctrlR = new ControladorRelatorioAcesso();
-    
-   
-   private ControladorRelatorioAcesso(){
-       this.telaRelatorioAcesso = new TelaRelatorioAcesso(this);
-       this.telasCabecalho = new TelasCabecalho();
-      
-   }
-   public void setControladorP(ControladorPrincipal CtrlP) {
-        this.ctrlP = CtrlP;
-    
-   }   
-   
-   public void exibeTelaRelatorioAcesso(){
-       telaRelatorioAcesso.mostraRelatorioAcesso();
-   }
-   
     public void cabecalhoTelaRelatorioAcesso() {
         telasCabecalho.cabecalhoTelaRelatorioAcesso();
     }
-
-    public void voltarTelaInicial() {
-        ctrlP.inicia();
-    }
-    
-  
-    
-   // public ArrayList <Acesso> getlistaLogAcesso();
-      //  return listaLogAcesso;
-    
-    //public void setlistaLogAcesso(ArrayList <Acesso> listaAcessos){
-        
-        //this.listaAcessos = listaAcessos;
-   // }
-
-    public void pedirListaEmprestimos() {
-        ctrlP.pedirListaEmprestimos();
-    }
-*/
-
-     
+   }
+       
 
     
 
